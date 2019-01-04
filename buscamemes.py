8c89ed10335bc969a1253538f05183915b4a915a
Felipe Lizama <felipealonsolr@gmail.com>
2019-01-04 03:34:19 -0300

    ahora rankea las 10 imagenes mas cercanas, dice su categoria y nombre

--- buscamemes.py
+++ buscamemes.py
@@ -3,6 +3,7 @@ import numpy as np
 import os
 import math
 import glob
+import heapq
 from sklearn.metrics.pairwise import cosine_similarity
 
 categorias = ["blink", "gru", "pikachu", "mono", "drake", "lisa", "outstanding move", "spiderman"]
@@ -17,31 +18,30 @@ def main():
             images.append([cv2.cvtColor(cv2.imread(os.path.join(path, file)), cv2.COLOR_BGR2GRAY), i, file])
     for tupla in images:
         tupla[0] = cv2.resize(tupla[0], (128, 128))
-    query = images[0]
-    #target = images[5]
-    #print("comparando " + target[2] + " de la categoria " + categorias[target[1]])
-    #sift = cv2.SIFT()
+    query = images[4]
+    print("La imagen query " + query[2])
+    sap = 131
+    print(images[sap][2] + " ", categorias[images[sap][1]])
+    #Usaremos SIFT
     sift = cv2.xfeatures2d.SIFT_create()
+    #par keypoints, descriptor de la query
     kp1, des1 = sift.detectAndCompute(query[0], None)
-    #des1 = des1.flatten()
-    #des2 = des2.flatten()
-    #print(len(des1))
-    #print(len(des2))
-    #sim = cosine_similarity([des1, des2])
-    #print(sim)
-    #kp = sift.detect(query[0], None)
-    #print(type(kp))
-    #bf = cv2.BFMatcher(cv2.NORM_L2, crossCheck=True)
-    #matches = bf.match(des1,des2)
+    #Contador para saber que numero del arreglo le corresponde a que foto
+    heap = []
+    heapq.heapify(heap)
+    cont= 0
     for target in images:
         if target[2] == query[2]:
             continue
+        #keypoint, descriptor de la imagen target
         kp2, des2 = sift.detectAndCompute(target[0], None)
         index_params = dict(algorithm=0, trees=5)
         search_params = dict()
         flann = cv2.FlannBasedMatcher(index_params, search_params)
+        #todos los matches
         matches = flann.knnMatch(des1, des2, k=2)
         good_points = []
+        #Encontramos las correspondencias
         for m, n in matches:
             if m.distance < 0.6*n.distance:
                 good_points.append(m)
@@ -50,15 +50,22 @@ def main():
             number_keypoints = len(kp1)
         else:
             number_keypoints = len(kp2)
-        #print("Keypoints 1ST Image: " + str(len(kp1)))
-        #print("Keypoints 2ND Image: " + str(len(kp2)))
-        #print("GOOD Matches:", len(good_points))
-        print(str(target[1]) +" "+ target[2] + " How good it's the match: ", len(good_points) / number_keypoints * 100)
-    #print(des1)
-    #print(des1.shape)
-    #print(len(matches))
-    #for m in matches:
-        #print(m.distance)
-    #print(matches)
+        #Creo que esto no es exactamente similitud, pero lo llamare similitud, hay que echarle un ojo
+        #la similitud y el nombre de la imagen correspondiente a la similitud
+        similitud = [len(good_points) / number_keypoints * 100, target[2], categorias[target[1]]]
+        print("nro " + str(cont) + " " + str(target[1]), similitud[0])
+        if len(heap) < 10:
+            heapq.heappush(heap, similitud)
+        else:
+            if heap[0][0] < similitud[0]:
+                heapq.heapreplace(heap, similitud)
+        cont+=1
+    print("El nombre de la imagen de query es:")
+    print(query[2])
+    print("Pertenece a la clase:")
+    print(categorias[query[1]])
+    print("Las 10 imagenes mas cercanas son")
+    for elem in heap:
+        print(elem)
 
 main()
